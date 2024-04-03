f9c4603eb45ac4c53768e635e1442acf6dd78866
rafiki1990 <blaiserafiki84@gmail.com>
2024-04-03 12:48:03 +0200

    done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0-validate_utf8 module."""
+
+
+def validUTF8(data):
+    """Check if dataset is a valid utf-8."""
+    try:
+        for num in data:
+            if num < 0 or num > 255:
+                return False
+        bytes_data = bytes(data)
+        bytes_data.decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
